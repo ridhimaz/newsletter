242146878ff0cf910bbeae29bf33f6dcee5cfded
Ridhima Kohli <ridhimakohli19@gmail.com>
2023-06-27 09:38:06 +0530

    layout change

--- index.js
+++ index.js
@@ -1,21 +1,8 @@
 const pattern = "^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$";
-
-// function checkMail()
-// {
-//     let value=document.getElementById('exampleInputEmail1').value;
-
-//     console.log(value.match(pattern));
-//     console.log(value);
-// }
-// function checkMail() {
-//     var email = document.getElementById('exampleInputEmail1').value;
-//     var filter = "^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$";
-//     if (!filter.test(email.value)) {
-//         alert('Please provide a valid email address');
-//         email.focus;
-//         return false;
-//     }
-// }
+function dismiss()
+{
+  window.location.reload();
+}
 function red()
 {
     document.getElementById("small-p").style.display = 'block';
@@ -24,7 +11,7 @@ function red()
 function thankYou()
 {
   document.querySelector(".container").style.display = 'none';
-  document.querySelector(".thankYou").style.visibility = 'visible';
+  document.querySelector(".thankYou").style.display = 'flex';
 }
 function checkMail() {
     const input = document.querySelector("#exampleInputEmail1");
